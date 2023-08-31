b16ad17011e501c382cb803015168914bfb8f6cf
Chris <c.r.zambito@gmail.com>
2023-08-31 17:19:11 -0400

    added tailwind

--- ghi/src/App.tsx
+++ ghi/src/App.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+
   return (
     <>
       <div>
+        <h1 className='text-5xl font-bold underline'>TEST</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
