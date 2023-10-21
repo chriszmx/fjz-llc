e723cec70654190752190a44a6cf6789ac1218af
Chris <c.r.zambito@gmail.com>
2023-10-21 14:58:37 -0400

    update darkmode for app form and mobile response

--- ghi/src/components/application/Application.jsx
+++ ghi/src/components/application/Application.jsx
@@ -22,7 +22,7 @@ const Application = () => {
     }
 
     return (
-        <div className="bg-gray-100 min-h-screen p-8">
+        <div className="bg-gray-100 min-h-screen p-8 dark:bg-gray-900">
             <h1 className="text-2xl font-bold mb-8">Application Form</h1>
             <FormTemplate user={user} />
         </div>
@@ -160,15 +160,15 @@ const FormTemplate = ({ user }) => {
     return (
         <form onSubmit={handleFormSubmit}>
             {sections.map((section, index) => (
-                <div key={index} className="mb-6 border p-4 rounded-md shadow-sm">
+                <div key={index} className="mb-6 border dark:border-gray-700 p-4 rounded-md shadow-sm bg-white dark:bg-gray-800">
                     <button
                         onClick={() => toggleSection(index)}
-                        className="flex justify-between items-center w-full bg-indigo-500 text-white px-4 py-2 rounded-md focus:outline-none"
+                        className="flex justify-between items-center w-full bg-indigo-500 hover:bg-indigo-600 dark:bg-indigo-600 dark:hover:bg-indigo-700 text-white px-4 py-2 rounded-md focus:outline-none"
                     >
                         <span>{section.title}</span>
                         <span>{section.isOpen ? '−' : '+'}</span>
                     </button>
-                    <p className="mt-2 text-gray-600">{section.description}</p>
+                    <p className="mt-2 text-gray-600 dark:text-gray-300">{section.description}</p>
                     {section.isOpen && (
                         <div className="mt-4 space-y-4">
                             {section.fields.map(field => (
@@ -185,7 +185,7 @@ const FormTemplate = ({ user }) => {
             ))}
             <button
                 type="submit"
-                className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md"
+                className="bg-green-500 hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700 text-white px-4 py-2 rounded-md"
             >
                 Submit
             </button>
@@ -204,7 +204,7 @@ const InputField = ({ field, value, onChange }) => {
                 />
             ) : (
                 <input
-                    className="border w-full p-2 rounded-md"
+                    className="border dark:border-gray-600 w-full p-2 rounded-md dark:bg-gray-700 dark:text-gray-300"
                     placeholder={field.name}
                     type={field.type}
                     required={field.required}
@@ -216,227 +216,4 @@ const InputField = ({ field, value, onChange }) => {
     );
 };
 
-
-
-
 export default Application;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
-// import { getFirestore, collection, addDoc } from "firebase/firestore";
-// import { app } from "../../Firebase";
-// import Login from "../../components/login/Login";
-// import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-
-// const Application = () => {
-//     const [user, setUser] = useState(null);
-//     const auth = getAuth(app);
-
-//     useEffect(() => {
-//         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-//             setUser(currentUser);
-//         });
-
-//         return () => unsubscribe();
-//     }, [auth]);
-
-//     if (!user) {
-//         return <Login />;
-//     }
-
-//     return (
-//         <div className="bg-gray-100 min-h-screen p-8">
-//             <h1 className="text-2xl font-bold mb-8">Application Form</h1>
-//             <FormTemplate user={user} />
-//         </div>
-//     );
-// };
-
-// const FormTemplate = ({ user }) => {
-//     const [sections, setSections] = useState([
-//         {
-//             title: 'Personal Information',
-//             description: 'Tell us a bit about yourself.',
-//             isOpen: true,
-//             fields: [
-//                 { name: "Full Name", type: "text", required: true },
-//                 { name: "Date of Birth", type: "date", required: true },
-//                 { name: "Phone Number", type: "tel", required: true }
-//             ]
-//         },
-
-//         {
-//             title: 'Identity Verification',
-//             description: 'Please upload your identification for verification purposes.',
-//             isOpen: false,
-//             fields: [
-//                 { name: "ID Proof", type: "file", required: true }
-//             ]
-//         },
-
-//         {
-//             title: 'Proof of Income 1',
-//             description: 'Please upload your Proof of Income (1) for verification purposes.',
-//             isOpen: false,
-//             fields: [
-//                 { name: "ID Proof", type: "file", required: true }
-//             ]
-//         },
-
-//         {
-//             title: 'Proof of Income 2',
-//             description: 'Please upload your Proof of Income (2) for verification purposes.',
-//             isOpen: false,
-//             fields: [
-//                 { name: "ID Proof", type: "file", required: true }
-//             ]
-//         }
-
-//     ]);
-
-//     const [formData, setFormData] = useState({});
-
-//     const handleInputChange = (name, value, e) => {
-//         if (name === "ID Proof") {
-//             const file = e.target.files[0];
-//             setFormData(prev => ({
-//                 ...prev,
-//                 [name]: file
-//             }));
-//         } else {
-//             setFormData(prev => ({
-//                 ...prev,
-//                 [name]: value
-//             }));
-//         }
-//     };
-
-
-//     const handleFormSubmit = async () => {
-//         try {
-//             const db = getFirestore(app);
-//             const storage = getStorage(app);
-
-//             let imageUrl = '';
-
-//             // Check if there's a file to upload
-//             if (formData["ID Proof"]) {
-//                 const storageRef = ref(storage, 'applications/' + formData["ID Proof"].name);
-//                 const uploadTask = uploadBytesResumable(storageRef, formData["ID Proof"]);
-
-//                 // Wait for the upload to complete
-//                 await new Promise((resolve, reject) => {
-//                     uploadTask.on('state_changed',
-//                         (snapshot) => {},
-//                         (error) => {
-//                             reject(error);
-//                         },
-//                         async () => {
-//                             imageUrl = await getDownloadURL(uploadTask.snapshot.ref);
-//                             resolve();
-//                         }
-//                     );
-//                 });
-
-//                 // Remove the File object from formData and replace with imageUrl
-//                 delete formData["ID Proof"];
-//             }
-
-//             await addDoc(collection(db, "application"), {
-//                 uid: user.uid,
-//                 email: user.email,
-//                 status: "pending",
-//                 image: imageUrl, // Store the download URL in Firestore
-//                 ...formData
-//             });
-
-//             alert('Form submitted successfully!');
-//         } catch (error) {
-//             console.error("Error submitting form: ", error);
-//             alert('There was an issue submitting the form.');
-//         }
-//     };
-
-
-
-//     const toggleSection = (index) => {
-//         const updatedSections = sections.map((section, idx) => {
-//             if (idx === index) {
-//                 return { ...section, isOpen: !section.isOpen };
-//             }
-//             return section;
-//         });
-//         setSections(updatedSections);
-//     };
-
-//     return (
-//         <div>
-//             {sections.map((section, index) => (
-//                 <div key={index} className="mb-6 border p-4 rounded-md shadow-sm">
-//                     <button
-//                         onClick={() => toggleSection(index)}
-//                         className="flex justify-between items-center w-full bg-indigo-500 text-white px-4 py-2 rounded-md focus:outline-none"
-//                     >
-//                         <span>{section.title}</span>
-//                         <span>{section.isOpen ? '−' : '+'}</span>
-//                     </button>
-//                     <p className="mt-2 text-gray-600">{section.description}</p>
-//                     {section.isOpen && (
-//                         <div className="mt-4 space-y-4">
-//                             {section.fields.map(field => (
-//                                 <InputField
-//                                     key={field.name}
-//                                     field={field}
-//                                     value={formData[field.name] || ""}
-//                                     onChange={e => handleInputChange(field.name, e.target.value, e)}
-//                                 />
-//                             ))}
-
-//                         </div>
-//                     )}
-//                 </div>
-//             ))}
-//             <button onClick={handleFormSubmit} className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md">Submit</button>
-//         </div>
-//     );
-// };
-
-// const InputField = ({ field, value, onChange }) => {
-//     return (
-//         <div className="mb-4">
-//             {field.type === "file" ? (
-//                 <input
-//                     type={field.type}
-//                     required={field.required}
-//                     onChange={onChange}
-//                 />
-//             ) : (
-//                 <input
-//                     className="border w-full p-2 rounded-md"
-//                     placeholder={field.name}
-//                     type={field.type}
-//                     required={field.required}
-//                     value={value}
-//                     onChange={onChange}
-//                 />
-//             )}
-//         </div>
-//     );
-// };
-
-
-
-
-// export default Application;
